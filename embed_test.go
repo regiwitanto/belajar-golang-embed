0461dd13738964d9dbf954514bd46536effc5c8e
regi <witantoregi@gmail.com>
2025-10-01 13:37:15 +0700

    Add embed test file with version variables and test function

---
+++ embed_test.go
@@ -0,0 +1,18 @@
+package belajar_golang_embed
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+//go:embed version.txt
+var version2 string
+
+func TestString(t *testing.T) {
+	fmt.Println(version)
+	fmt.Println(version2)
+}
